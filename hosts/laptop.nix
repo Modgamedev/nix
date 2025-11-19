81ef60782e0ec58de18d77f2c609f89ac07b2f16
Modgamedev <modgamedev@gmail.com>
2025-11-19 14:50:03 +0500

    Update laptop.nix

--- hosts/laptop.nix
+++ hosts/laptop.nix
@@ -2,11 +2,15 @@
 
 {
   imports = [
+    ./hardware-configuration.nix  # конфиг разделов диска и файловых систем
     ./modules/users.nix   # модуль пользователя
   ];
 
   networking.hostName = "nixos-laptop";
   system.stateVersion = "25.05";
-
   time.timeZone = "UTC";
+
+  # Настройка загрузчика
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 }
