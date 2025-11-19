e1981f6e14e3b9e18efded40a969f12ed5cf7899
Modgamedev <modgamedev@gmail.com>
2025-11-19 14:27:17 +0500

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "NixOS + Flakes + HomeManager";
+  
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+    in 
+   
+    {
+      nixosConfigurations = {
+        laptop = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./hosts/laptop.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = { 
+                useGlobalPkgs = true; 
+                useUserPackages = true; 
+                backupFileExtension = "backup"; 
+                users.mod = import ./home/laptop.nix; 
+              };
+            }
+          ];
+        };
+        server = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./hosts/server.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = { 
+                useGlobalPkgs = true; 
+                useUserPackages = true; 
+                backupFileExtension = "backup"; 
+                users.mod = import ./home/server.nix; 
+              };
+            }
+          ];
+        };
+      };
+
+
