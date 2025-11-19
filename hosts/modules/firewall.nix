aca545c6c8806ac2f8b92d4148000199f2f0e028
Modgamedev <modgamedev@gmail.com>
2025-11-19 13:29:52 +0500

    Create firewall.nix

---
+++ hosts/modules/firewall.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }:
+
+{
+  networking.firewall = {
+    enable = true;                    # Включаем фаервол
+    package = pkgs.nftables;          # Используем nftables вместо iptables
+    allowedTCPPorts = [ 58585 ];      # Открытые TCP порты
+    allowedUDPPorts = [  ];           # Открытые UDP порты
+    allowPing = false;                # Отключаем пинги
+  }
+}
