8c8e0d21d78a2cc725ea6d98f01c714af4d11b45
Modgamedev <modgamedev@gmail.com>
2025-11-20 16:03:46 +0500

    Update tty.nix

--- hosts/modules/tty.nix
+++ hosts/modules/tty.nix
@@ -2,9 +2,8 @@
 
 {
   services.getty = {
-    greetingLine = lib.mkForce "NixOS ${config.system.nixos.release} - \l";
-''NixOS ${config.system.nixos.release} - \l'';
-    helpLine = lib.mkForce "";
+    greetingLine = lib.mkForce "NixOS ${config.system.nixos.release} - \l";    # Выводим сообщение о канале NixOS и номере tty
+    helpLine = lib.mkForce "";                                                 # Убираем helpline
   };
   console = {
     enable = true;         # Включен по умолчанию, но пусть будет
