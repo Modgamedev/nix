3255e78afcd2532b6fcc4c4919c56cabfeceb548
xymod <modgamedev@gmail.com>
2025-11-28 12:49:41 +0000

    Update swaylock.nix

--- home/modules/swaylock.nix
+++ home/modules/swaylock.nix
@@ -1,1 +1,16 @@
+{ config, pkgs, lib, ... }:
 
+{
+  services.swayidle = {
+    enable = true;
+  };
+
+  programs.swaylock = {
+    enable = true;
+    settings = {
+      color = "#1c1c1c";
+      indicator = true;
+      ring-color = "#a3be8c";
+    };
+  };
+}
